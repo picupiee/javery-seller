14333fd0ab4b2b9b4a32cf1c03b2d00b90d5f124
picupiee <105708684+picupiee@users.noreply.github.com>
2025-10-13 20:09:31 +0700

    deleting configuration, api and private key in firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,4 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBovbTv6dVFJSb-jR8C9LZqPjBzWS49AxI",
-  authDomain: "javery-72191.firebaseapp.com",
-  projectId: "javery-72191",
-  storageBucket: "javery-72191.firebasestorage.app",
-  messagingSenderId: "417678757547",
-  appId: "1:417678757547:web:b1626cd0374383c83271ee",
-  measurementId: "G-X3149815YE"
-}
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+// Deleted, please check up firebase for a new configuration.
