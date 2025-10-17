7c857a61d7e7130481ee36faae84b84e185a71b9
picupiee <picupiee.id@gmail.com>
2025-10-17 21:09:20 +0700

    adding extra to app.json, modify firebase as well

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,30 @@
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import {getAnalytics} from "firebase/analytics"
 
+type ExpoExtraConfig = {[key: string]: any} | undefined
+
+const getConfigValue = (key: string): string => {
+const extraConfig = Constants.expoConfig?.extra as ExpoExtraConfig
+const value = process.env[key]
+?? extraConfig?.[key]
+
+if (!value || typeof value !== 'string') {
+  throw new Error(`Firebase key ${key} is missing, undefined or not a string. Check your .env file`)
+}
+return value
+}
+
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: getConfigValue('EXPO_PUBLIC_FIREBASE_API_KEY'),
+  authDomain: getConfigValue('EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN'),
+  projectId: getConfigValue('EXPO_PUBLIC_FIREBASE_PROJECT_ID'),
+  storageBucket: getConfigValue('EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET'),
+  messagingSenderId: getConfigValue('EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'),
+  appId: getConfigValue('EXPO_PUBLIC_FIREBASE_APP_ID'),
+  measurementId: getConfigValue('EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID')
 };
 
 const app = initializeApp(firebaseConfig)
